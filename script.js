9d1ec34d418b267e90034d3df541df51cfcb8c98
thiccbob1 <samplejesse1@gmail.com>
2022-11-09 18:42:02 -0600

    added etch functionality

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-let length = 17;
+let length = 16;
+var div;
 let mainContainer = document.getElementById('mainContainer');
+let sizeButton = document.getElementById('sizeButton');
+let resetButton = document.getElementById('resetButton');
 
 function gridMaker(){
     let width = 500/length;
     for (let i = 1; i <= (length*length); i++){
-        var div = document.createElement('div');
+        div = document.createElement('div')
         div.setAttribute('id', 'etchDiv');
-        div.setAttribute('style', 'border: 1px solid black');
+        div.setAttribute('style', 'border: 1px solid black;');
+        div.style.backgroundColor = ("white");
         div.style.width = width + "px";
         div.style.height = width + "px";
         mainContainer.appendChild(div);
-        console.log(i);
     }
+    let divs = document.querySelectorAll('#etchDiv');
+    divs.forEach(divs => {
+        divs.addEventListener('mouseover', () => {
+        divs.style.backgroundColor = "black";
+    })
+});
 }
 
 gridMaker();
+
+function changeSize(){
+    length = parseInt(prompt("Grid Size: "))
+    while (mainContainer.firstChild){
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+    gridMaker();
+}
+
+function reset(){
+    while (mainContainer.firstChild){
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+    gridMaker();
+}
+
+sizeButton.addEventListener('click', changeSize);
+resetButton.addEventListener('click', reset);
